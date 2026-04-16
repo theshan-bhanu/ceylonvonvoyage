853ee3041a821959ffde3c83eb841699ea3162bd
theshan-bhanu <theshanbhanuka@gmail.com>
2026-04-16 14:58:40 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/srilanka_tourism/',
   plugins: [
     tailwindcss(),
   ],
